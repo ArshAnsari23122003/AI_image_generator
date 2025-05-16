f5e88b5f84f14d8bbc7368c4b99e582b22028349
ArshAnsari23122003 <arsh.nitw@gmail.com>
2025-05-16 22:34:49 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.send('Hello from DALL-E!');
+});
+
+const startServer = () => {
+  try {
+    app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
